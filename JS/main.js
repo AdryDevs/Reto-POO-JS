53296af506d11456952b16984dd72caa69b8f65a
AdryDevs <alonsoadrian86@gmail.com>
2022-10-07 13:58:55 +0200

    retos 1 y 2 realizados

---
+++ JS/main.js
@@ -0,0 +1,48 @@
+//Reto 1
+
+// const sex = "hombre";
+
+// class Persona {
+    
+//     constructor(nombre,DNI,edad,peso,altura){
+//         this.DNI = DNI;
+//         this.nombre= nombre;
+//         this.edad= edad;
+//         this.peso = peso;
+//         this.altura = altura;
+//         this.sexo = sex;
+//     }
+// };
+
+// let Persona1 = new Persona("Adrian",20843404,36,79,177);
+// let Persona2 = new Persona("Javi",44527964,27,62,171);
+// let Persona3 = new Persona("Alex",48407737,29,70,170);
+
+// console.log(Persona1);
+// console.log(Persona2);
+// console.log(Persona3);
+
+//Reto 2
+
+// let contraseña = [];
+
+// let longitud = 99999999
+
+// class Password {
+//     constructor(contraseña){
+//         this.longitud;
+//         this.contraseña = "1234";
+        
+//     }     
+    
+//     createPassword(){
+//         this.contraseña = parseInt(Math.random() * longitud);
+
+//     }
+// }
+
+// let contraseña1 = new Password;
+
+// contraseña1.createPassword();
+
+// console.log(contraseña1);
